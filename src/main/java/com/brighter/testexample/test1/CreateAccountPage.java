e1a42551a1d1c2ac35d76c4f2f6d157791853f29
padmajab2017 <31637027+padmajab2017@users.noreply.github.com>
2017-09-25 10:39:57 -0400

    First Commit

---
+++ src/main/java/com/brighter/testexample/test1/CreateAccountPage.java
@@ -0,0 +1,33 @@
+package com.brighter.testexample.test1;
+
+	import org.openqa.selenium.WebDriver;
+
+	public class CreateAccountPage 
+	{
+		WebDriver driver;
+
+
+	public String getUrl()
+	{
+		
+		return driver.getCurrentUrl();
+		
+	}
+
+
+
+
+
+		public  CreateAccountPage(WebDriver driver)
+		{
+			this.driver=driver;
+		}
+
+		
+		
+		
+		
+		
+	}
+
+
---
+++ src/main/java/com/brighter/testexample/test1/HomePage.java
@@ -0,0 +1,40 @@
+package com.brighter.testexample.test1;
+
+	import org.openqa.selenium.By;
+	import org.openqa.selenium.WebDriver;
+	import org.openqa.selenium.support.ui.ExpectedConditions;
+	import org.openqa.selenium.support.ui.WebDriverWait;
+
+	public class HomePage
+	{
+		WebDriver driver;
+
+	public void clickOnGmail()
+	{
+		WebDriverWait wait= new WebDriverWait(driver, 15);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("gb_P"))).click();
+		
+		
+	}
+
+	public void clickCreateAccount()
+	{
+		
+		WebDriverWait wait= new WebDriverWait(driver, 15);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("CREATE AN ACCOUNT"))).click();
+		System.out.println("This is first Change");
+		
+	}
+
+
+
+
+
+		public HomePage(WebDriver driver)
+		{
+			this.driver=driver;
+		}
+
+	}
+
+
