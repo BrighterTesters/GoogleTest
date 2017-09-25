32cc442940ce2fd8ddc2244b17d88d4dba3aee14
padmajab2017 <31637027+padmajab2017@users.noreply.github.com>
2017-09-25 15:35:35 -0400

    Change in Test

--- src/test/java/com/brighter/testexample/test1/GoogleTestCase.java
+++ src/test/java/com/brighter/testexample/test1/GoogleTestCase.java
@@ -4,7 +4,9 @@ import static org.testng.Assert.assertTrue;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
